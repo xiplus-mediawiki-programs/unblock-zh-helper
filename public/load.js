b8024eb32f519e81dd76f47f264dde0f0641dc36
xiplus <huangxuanyuxiplus@gmail.com>
2022-03-02 17:08:22 +0800

    Update load.js

--- public/load.js
+++ public/load.js
@@ -1,8 +1,16 @@
-mw.loader.load('http://localhost:3000/dist/style.css', 'text/css');
-mw.loader.load('http://localhost:3000/dist/unblock-zh-helper.iife.js');
+/* eslint indent: ["error", "tab"] */
+if (
+	mw.config.get('wgCanonicalSpecialPageName') === 'Blankpage' &&
+	/\/unblock-zh-helper$/.test(mw.config.get('wgPageName'))
+) {
+	$('#firstHeading').text('unblock-zh-helper');
+	$('title').text('unblock-zh-helper');
+	$('#mw-content-text').html('<div id="app"></div>');
 
-$('#firstHeading').text('unblock-zh-helper')
-$('title').text('unblock-zh-helper')
-$('#mw-content-text').html('<div id="app"></div>')
+	mw.loader.using(['ext.gadget.site-lib', 'ext.gadget.morebits', 'mediawiki.api'], function () {
+		mw.loader.load('https://zh.wikipedia.org/w/index.php?title=User:Xiplus/js/unblock-zh-helper/style.css&action=raw&ctype=text/css', 'text/css');
+		mw.loader.load('https://zh.wikipedia.org/w/index.php?title=User:Xiplus/js/unblock-zh-helper/unblock-zh-helper.iife.js&action=raw&ctype=text/javascript');
+	});
+}
 
 // mw.loader.load('http://localhost:3000/dist/load.js');
